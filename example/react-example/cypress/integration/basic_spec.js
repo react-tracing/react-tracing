1d1733d76461708efe82310b145fe1cd7170f016
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-10-25 14:49:42 +0200

    Add test involving open zipkin (#6)
    * add test for quering an open zipkin instance
    
    * let CI fail if tests fail
    
    * add babel-polyfill
    
    * use chai syntax

--- example/react-example/cypress/integration/basic_spec.js
+++ example/react-example/cypress/integration/basic_spec.js
@@ -1,13 +1,25 @@
+require("babel-polyfill");
+import fetch from "node-fetch";
+
 describe("Basic", () => {
-  it("should be able interact with the basic example", () => {
-    cy.visit("/");
-    cy.get("#Basic").click();
-    cy.get("#buttonLabel").should($p => {
-      expect($p.first()).to.contain("Not-Pressed");
+  describe("Setup", () => {
+    it("should be able interact with the basic example", () => {
+      cy.visit("/");
+      cy.get("#Basic").click();
+      cy.get("#buttonLabel").should($p => {
+        expect($p.first()).to.contain("Not-Pressed");
+      });
+      cy.get("#basicButton").click();
+      cy.get("#buttonLabel").should($p => {
+        expect($p.first()).to.contain("Is-Pressed");
+      });
     });
-    cy.get("#basicButton").click();
-    cy.get("#buttonLabel").should($p => {
-      expect($p.first()).to.contain("Is-Pressed");
+
+    it("should be able to access zipkin", async () => {
+      const result = await fetch(
+        "http://localhost:9411/api/v1/spans?serviceName=frontend"
+      ).then(res => res.json());
+      expect(result).to.eql([]);
     });
   });
 });
