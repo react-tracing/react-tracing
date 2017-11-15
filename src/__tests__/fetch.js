9daf6de82502ff1eb2bed017c0665e29fe8951a9
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-11-15 14:04:08 +0100

    refactor(tests): remove describe block

--- src/__tests__/fetch.js
+++ src/__tests__/fetch.js
@@ -13,7 +13,7 @@ class MockTracer {
   }
 }
 
-describe("requests", () => {
+describe("requests - fetch", () => {
   let tracer, fetchMock, finishMock, succeedRequest, failRequest;
 
   beforeEach(() => {
@@ -28,77 +28,75 @@ describe("requests", () => {
     );
   });
 
-  describe("fetch", () => {
-    it("tracer should expose fetch", () => {
-      expect(tracer.fetch).toBeInstanceOf(Function);
-    });
+  it("tracer should expose fetch", () => {
+    expect(tracer.fetch).toBeInstanceOf(Function);
+  });
 
-    it("should throw if there is no fetch to be found", () => {
-      expect(() => {
-        tracer.fetch();
-      }).toThrowError();
-      expect(() => {
-        tracer.fetch({});
-      }).toThrowError();
+  it("should throw if there is no fetch to be found", () => {
+    expect(() => {
+      tracer.fetch();
+    }).toThrowError();
+    expect(() => {
+      tracer.fetch({});
+    }).toThrowError();
 
-      expect(() => {
-        tracer.fetch({ fetch: fetchMock });
-      }).not.toThrowError();
+    expect(() => {
+      tracer.fetch({ fetch: fetchMock });
+    }).not.toThrowError();
 
-      const oldGlobal = global;
-      global.fetch = fetchMock;
-      expect(() => {
-        tracer.fetch();
-        tracer.fetch({});
-      }).not.toThrowError();
-      expect(() => {
-        tracer.fetch({ fetch: fetchMock });
-      }).not.toThrowError();
-    });
+    const oldGlobal = global;
+    global.fetch = fetchMock;
+    expect(() => {
+      tracer.fetch();
+      tracer.fetch({});
+    }).not.toThrowError();
+    expect(() => {
+      tracer.fetch({ fetch: fetchMock });
+    }).not.toThrowError();
+  });
 
-    it("tracer should start and finish a span", async () => {
-      const instrumentedFetch = tracer.fetch({ fetch: fetchMock });
-      const promise = instrumentedFetch("http://google.de");
-      expect(tracer.stack.list.length).toBe(1);
-      expect(tracer.stack.list[0].name).toBe("http://google.de-GET");
+  it("tracer should start and finish a span", async () => {
+    const instrumentedFetch = tracer.fetch({ fetch: fetchMock });
+    const promise = instrumentedFetch("http://google.de");
+    expect(tracer.stack.list.length).toBe(1);
+    expect(tracer.stack.list[0].name).toBe("http://google.de-GET");
 
-      succeedRequest();
+    succeedRequest();
+    await promise;
+
+    expect(finishMock).toHaveBeenCalled();
+    expect(tracer.stack.list.length).toBe(0);
+  });
+
+  it("tracer should start and finish a span if an error is thrown", async () => {
+    const instrumentedFetch = tracer.fetch({ fetch: fetchMock });
+
+    const promise = instrumentedFetch("http://google.de");
+    expect(tracer.stack.list.length).toBe(1);
+    failRequest();
+
+    try {
       await promise;
+      fail("Promise should not succeed");
+    } catch (e) {}
 
-      expect(finishMock).toHaveBeenCalled();
-      expect(tracer.stack.list.length).toBe(0);
+    expect(finishMock).toHaveBeenCalled();
+    expect(tracer.stack.list.length).toBe(0);
+  });
+
+  it("should use a different span name if a getSpanName function is provided", async () => {
+    const instrumentedFetch = tracer.fetch({
+      fetch: fetchMock,
+      getSpanName: () => "ponies"
     });
+    const promise = instrumentedFetch("http://google.de");
+    expect(tracer.stack.list.length).toBe(1);
+    expect(tracer.stack.list[0].name).toBe("ponies");
 
-    it("tracer should start and finish a span if an error is thrown", async () => {
-      const instrumentedFetch = tracer.fetch({ fetch: fetchMock });
+    succeedRequest();
+    await promise;
 
-      const promise = instrumentedFetch("http://google.de");
-      expect(tracer.stack.list.length).toBe(1);
-      failRequest();
-
-      try {
-        await promise;
-        fail("Promise should not succeed");
-      } catch (e) {}
-
-      expect(finishMock).toHaveBeenCalled();
-      expect(tracer.stack.list.length).toBe(0);
-    });
-
-    it("should use a different span name if a getSpanName function is provided", async () => {
-      const instrumentedFetch = tracer.fetch({
-        fetch: fetchMock,
-        getSpanName: () => "ponies"
-      });
-      const promise = instrumentedFetch("http://google.de");
-      expect(tracer.stack.list.length).toBe(1);
-      expect(tracer.stack.list[0].name).toBe("ponies");
-
-      succeedRequest();
-      await promise;
-
-      expect(finishMock).toHaveBeenCalled();
-      expect(tracer.stack.list.length).toBe(0);
-    });
+    expect(finishMock).toHaveBeenCalled();
+    expect(tracer.stack.list.length).toBe(0);
   });
 });
