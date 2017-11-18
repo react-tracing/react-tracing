1ba034af6f9058d4de8f4d009ed7f62afb3e3238
Daniel Schmidt <danielmschmidt92@gmail.com>
2017-11-18 19:02:46 +0100

    add implementation of (failing) second async scenario test

--- src/__tests__/async.js
+++ src/__tests__/async.js
@@ -103,5 +103,41 @@ describe("async stacks", () => {
   *  └──┬──────┘
   *    ▼
   */
-  it("should trace correcly with nested spans");
+  it("should trace correcly with nested spans", async () => {
+    let spanId = 0;
+    const getSpanName = () => {
+      spanId += 1;
+      return `span-${spanId}`;
+    };
+
+    const instrumentedFetch = tracer.fetch({ fetch: nodeFetch, getSpanName });
+    const server1 = await generateServer(100);
+    const server2 = await generateServer(25);
+    expect(tracer.stack.list.length).toBe(0);
+
+    // Fetch1
+    const fetch1 = instrumentedFetch(
+      `http://127.0.0.1:${server1.address().port}`
+    );
+    expect(tracer.stack.list.length).toBe(1);
+    expect(tracer.stack.peek().name).toBe("span-1");
+    await wait(10);
+
+    // Fetch2
+    const fetch2 = instrumentedFetch(
+      `http://127.0.0.1:${server2.address().port}`
+    );
+    expect(tracer.stack.list.length).toBe(2);
+    expect(tracer.stack.peek().name).toBe("span-2");
+
+    // Response2
+    await fetch2;
+    expect(tracer.stack.list.length).toBe(1);
+    expect(tracer.stack.peek().name).toBe("span-1");
+
+    // Response1
+    await fetch1;
+    expect(tracer.stack.list.length).toBe(0);
+    expect(finishMock).toHaveBeenCalledTimes(2);
+  });
 });
